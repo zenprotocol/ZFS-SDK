6a976c0609e61eb49d50332ef64371c5f4602403
Ash <ash@ash-ubuntu16.04>
2018-01-01 14:00:31 +0200

    refactor

--- Program.fs
+++ Program.fs
@@ -1,4 +1,25 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+﻿
+
+let usage =
+    """
+    USAGE: ZFS_SDK.exe --help
+           ZFS_SDK.exe <source file path> [< option >]
+    
+    PARAMS:
+        <source file path>      The ZF* source file to use
+    
+    OPTIONS:
+        --help, -h              display this list of options
+    """
+let showUsage() : unit =
+    printfn "%s" usage
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| "--help" |] -> showUsage()
+    | [| sourceFilePath |] -> showUsage()
+    | [| sourceFilePath; "-e" |] -> ()        
+    | _ -> showUsage()
+    
     0 // return an integer exit code

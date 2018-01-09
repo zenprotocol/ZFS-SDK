149aee843cb1b06bac1a78c1ccfde4612ce77cfa
Ash <ash@ash-ubuntu16.04>
2018-01-09 16:14:32 +0200

    Run almost working

--- Program.fs
+++ Program.fs
@@ -1,6 +1,37 @@
 ﻿
 open System.IO
+open System.Text
+open Microsoft.FSharp.Compiler.Interactive.Shell
 
+// returns the path for the folder containing the specified file. 
+let getDir : string -> string =
+    Path.GetFullPath >> Path.GetDirectoryName
+
+let (/) path1 path2 = Path.Combine(path1, path2)
+
+let elab_file (filepath:string) : unit =
+    let elaboratedDir = getDir filepath/"Elaborated"
+    Directory.CreateDirectory elaboratedDir |> ignore
+    let elaboratedFilePath = elaboratedDir/Path.GetFileName filepath
+    let ast = filepath |> ASTUtils.parse_file
+                       |> ASTUtils.elab_ast
+                       |> ASTUtils.add_main_to_ast
+    ASTUtils.write_ast_to_file ast elaboratedFilePath
+    printfn "Wrote elaborated source to %s" elaboratedFilePath
+
+let run_fsx (fsxFile : string) : Zen.Types.Extracted.transactionSkeleton =
+    let inStream = new StringReader("")
+    let defaultArgs = [|"fsi.exe";"--noninteractive";"--nologo";"--gui-" |]
+    let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+    let fsiSession = FsiEvaluationSession.Create(fsiConfig, defaultArgs, inStream, stdout, stderr) 
+    let fsx = File.ReadAllText fsxFile
+    match fsiSession.EvalExpression fsx with
+    | None -> 
+        failwithf "No result when running %s" fsxFile
+    | Some value -> 
+        value.ReflectionValue |> unbox<Consensus.TxSkeleton.TxSkeleton>
+                              |> Consensus.ZFStar.convertInput
+    
 let usage =
     """
     USAGE: ZFS_SDK.exe --help
@@ -19,23 +50,7 @@ let usage =
     """
 let showUsage() : unit =
     printfn "%s" usage
-
-// returns the path for the folder containing the specified file. 
-let getDir : string -> string =
-    Path.GetFullPath >> Path.GetDirectoryName
-
-let (/) path1 path2 = Path.Combine(path1, path2)
-
-let elab_file (filepath:string) : unit =
-    let elaboratedDir = getDir filepath/"Elaborated"
-    Directory.CreateDirectory elaboratedDir |> ignore
-    let elaboratedFilePath = elaboratedDir/Path.GetFileName filepath
-    let ast = filepath |> ASTUtils.parse_file
-                       |> ASTUtils.elab_ast
-                       |> ASTUtils.add_main_to_ast
-    ASTUtils.write_ast_to_file ast elaboratedFilePath
-    printfn "Wrote elaborated source to %s" elaboratedFilePath
-    
+   
 [<EntryPoint>]
 let rec main argv =
     match argv with
@@ -76,9 +91,38 @@ let rec main argv =
     
     | [| sourceFilePath; "-r"; inputFilePath |] ->
         main [|sourceFilePath; "-c" |] |> ignore
+        let odir =
+                        let dir = sourceFilePath |> Path.GetFullPath 
+                                                 |> Path.GetDirectoryName
+                        dir/"Elaborated"/"Output"
+        let dll = 
+            let dllFile = Path.ChangeExtension(Path.GetFileName sourceFilePath, ".dll")
+            odir / dllFile
+       
+        let contractHash =
+            sourceFilePath |> File.ReadAllText 
+                           |> System.Text.Encoding.UTF8.GetBytes
+                           |> Consensus.Hash.compute
+        
+        let contractAssembly = dll |> System.Reflection.Assembly.LoadFrom
+        let contractMainFunction = contractAssembly.GetModules().[0]
+                                                   .GetMethod("mainFunction")
+                                                   .Invoke(null, [||])
+                                                   :?> Zen.Types.Extracted.mainFunction
+        let contractCostFunction, contractMainFunction =
+            match contractMainFunction with 
+            | Zen.Types.Extracted.MainFunc (cf, mf) -> 
+                match cf with
+                | Zen.Types.Extracted.CostFunc(_, cf) -> cf, mf
+         
+        let inputTxSkeleton = 
+            //run_fsx inputFilePath
+            Consensus.ZFStar.convertInput Consensus.TxSkeleton.empty
+       
+        
         ()
-    
-    | _ -> 
+                     
+    | _ ->
         showUsage()
     
     0 // return an integer exit code
---
+++ Tests/simpleTxSkel.fsx
@@ -0,0 +1,6 @@
+//#I "../paket-files/gitlab.com/zenprotocol/zenprotocol/src/Consensus/bin/Debug"
+//#r "Consensus.dll"
+
+open Consensus.TxSkeleton
+
+empty

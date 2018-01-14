87f7cb1cc361509adea280eeb7217139460d78e8
Doron Somech <somdoron@gmail.com>
2018-01-14 15:38:55 +0200

    fix reference order in testContract.fsx

--- Tests/testContract.fsx
+++ Tests/testContract.fsx
@@ -1,14 +1,12 @@
-#I "bin"
-#r "Compile.Test.dll"
-#I "../bin/Debug/"
-#r "Consensus.dll"
-#r "Zulib.dll"
-#r "System.Reflection.Metadata.dll"
-#r "FSharp.Compatibility.OCaml.dll"
 #r "mscorlib.dll"
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
+#I "../bin/Debug/"
+#r "../bin/Debug/Consensus.dll"
+#r "../bin/Debug/Zulib.dll"
+#I "bin"
+#r "Compile.Test.dll"
 
 open Compile.Test
 open Zen.Types.Extracted

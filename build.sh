12f4cf9b4cde6aa815edca49c96f6c3ededf3343
Sharon Urban <urban_sharon@hotmail.com>
2018-03-24 16:06:19 +0300

    ensure Z3 has execute access permissions

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 # build the sdk
 mono .paket/paket.exe restore
 msbuild ZFS-SDK.sln /p:Configuration=Release
+
+# ensure Z3 has execute access permissions
+chmod +x packages/zen_z3_linux/output/z3
+chmod +x packages/zen_z3_osx/output/z3

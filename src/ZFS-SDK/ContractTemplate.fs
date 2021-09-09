8873baa88e19411f8f4f1e8b458e9c104f59d651
arielzenprotocol <ariel@zenprotocol.com>
2021-09-09 16:24:14 +0300

    feat: using the empty contract as a template in zebra create

--- src/ZFS-SDK/ContractTemplate.fs
+++ src/ZFS-SDK/ContractTemplate.fs
@@ -3,62 +3,25 @@ module ContractTemplate
 let template : Printf.StringFormat<string -> string> =
   "module %s
 
-open Zen.Types
-open Zen.Base
 open Zen.Cost
-open Zen.Asset
-open Zen.Data
+open Zen.Types
 
-module D = Zen.Dictionary
-module W = Zen.Wallet
-module RT = Zen.ResultT
-module Tx = Zen.TxSkeleton
-module C = Zen.Cost
 module CR = Zen.ContractResult
 
-let buy txSkeleton contractId returnAddress =
-  let! contractToken = Zen.Asset.getDefault contractId in
-  let! amount = Tx.getAvailableTokens zenAsset txSkeleton in
+let main txSkel context contractId command sender messageBody w state = // 15
+    CR.ofTxSkel txSkel
 
-  let! txSkeleton =
-    Tx.lockToContract zenAsset amount contractId txSkeleton
-    >>= Tx.mint amount contractToken
-    >>= Tx.lockToAddress contractToken amount returnAddress in
-
-  CR.ofTxSkel txSkeleton
-
-let redeem txSkeleton contractId returnAddress wallet =
-  let! contractToken = Zen.Asset.getDefault contractId in
-  let! amount = Tx.getAvailableTokens contractToken txSkeleton in
-
-  let! txSkeleton =
-    Tx.destroy amount contractToken txSkeleton
-    >>= Tx.lockToAddress zenAsset amount returnAddress
-    >>= Tx.fromWallet zenAsset amount contractId wallet in
-
-  CR.ofOptionTxSkel \"contract doesn't have enough zens tokens\" txSkeleton
-
-let main txSkeleton _ contractId command sender messageBody wallet state =
-  let! returnAddress =
-    messageBody >!= tryDict
-                >?= D.tryFind \"returnAddress\"
-                >?= tryLock
-  in
-  match returnAddress with
-  | Some returnAddress ->
-    if command = \"redeem\" then
-      redeem txSkeleton contractId returnAddress wallet
-    else if command = \"\" || command = \"buy\" then
-      buy txSkeleton contractId returnAddress
-      |> autoInc
-    else
-      RT.autoFailw \"unsupported command\"
-  | None ->
-    RT.autoFailw \"returnAddress is required\"
-
-let cf _ _ _ _ _ wallet _ =
-    4 + 64 + 2 + (64 + (64 + (64 + 64 + (Zen.Wallet.size wallet * 128 + 192) + 3)) + 25) + 31
-    |> C.ret #nat\n"
+val cf:
+       txSkel     : txSkeleton
+    -> context    : context
+    -> command    : string
+    -> sender     : sender
+    -> messageBody: option data
+    -> w          : wallet
+    -> state      : option data
+    -> nat `cost` 1
+let cf _ _ command _ _ w _ =
+    ret (4 <: nat)\n"
 
 let code (filename : string) =
     sprintf template (filename.Substring (0, filename.Length - 4))
